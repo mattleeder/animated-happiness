a72064bc71a2e2c41364db70ef7f633727e38f3d
unknown <matthewleeder@yahoo.co.uk>
2022-03-27 03:23:19 +0100

    -

--- cmdargparse.py
+++ cmdargparse.py
@@ -1,18 +1,19 @@
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--api_key',
-    help="Faceit Developer API key",
-    required=False
-)
-args = parser.parse_args()
-api_key = args.api_key
+# parser = argparse.ArgumentParser()
+# parser.add_argument(
+#     '--api_key',
+#     help="Faceit Developer API key",
+#     required=False
+# )
+# args = parser.parse_args()
+# api_key = args.api_key
 
-if api_key is None:
-    os.environ['API_KEY']
+# if api_key is None:
+#     os.environ['API_KEY']
 
+api_key = os.environ['API_KEY']
 
 def main():
     pass
