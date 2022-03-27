7b1fb833eb50454f5b4438bad60c88864af961bd
unknown <matthewleeder@yahoo.co.uk>
2022-03-27 02:33:12 +0100

    added os.environ api_key lookup

--- cmdargparse.py
+++ cmdargparse.py
@@ -1,12 +1,18 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    'api_key',
-    help="Faceit Developer API key"
+    '--api_key',
+    help="Faceit Developer API key",
+    required=False
 )
 args = parser.parse_args()
 api_key = args.api_key
+
+if api_key is None:
+    os.environ['API_KEY']
+
 
 def main():
     pass
